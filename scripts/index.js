8c56aa32be5709cac138c8cd3fb1279e3c3ad0c3
scottfulmer <117692506+scottfulmer@users.noreply.github.com>
2023-02-11 14:59:31 -0500

    like buttons all working, trash button visible but to only 1 picture, preview modal started in HTML & css

--- scripts/index.js
+++ scripts/index.js
@@ -44,7 +44,6 @@ const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
-
 /* -------------------------------------------------------------------------- */
 /*                                  Elements                                  */
 /* -------------------------------------------------------------------------- */
@@ -71,11 +70,18 @@ function closePopop() {
 
 function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
+
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardTitleEl = cardElement.querySelector(".card__title");
+  const likeButton = cardElement.querySelector(".card__like-button");
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card__like-button_active");
+  });
+
   cardTitleEl.alt = cardData.name;
   cardImageEl.src = cardData.link;
   cardTitleEl.textContent = cardData.name;
+
   return cardElement;
 }
 function openModal(modal) {
@@ -88,6 +94,7 @@ function renderCard(cardData, wrapper) {
   const cardElement = getCardElement(cardData);
   wrapper.prepend(cardElement);
 }
+
 /* -------------------------------------------------------------------------- */
 /*                               Event Handlers                               */
 /* -------------------------------------------------------------------------- */
