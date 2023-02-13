bc7b17c61a625f5b890b6dc6be117f90c0099cb3
scottfulmer <117692506+scottfulmer@users.noreply.github.com>
2023-02-12 20:49:42 -0500

    fixed query selector, images are now visible and everything is functioning fine except for the preview modal and trash button

--- scripts/index.js
+++ scripts/index.js
@@ -82,7 +82,7 @@ function getCardElement(cardData) {
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardTitleEl = cardElement.querySelector(".card__title");
   const likeButton = cardElement.querySelector(".card__like-button");
-  const deleteButton = cardElement.querySelectorAll(".card__delete-button");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
   likeButton.addEventListener("click", () => {
     likeButton.classList.toggle("card__like-button_active");
   });
