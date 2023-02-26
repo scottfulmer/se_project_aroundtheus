0c105b3a80693c42fe4338dca5e8663c39e66e93
scottfulmer <117692506+scottfulmer@users.noreply.github.com>
2023-02-25 20:04:28 -0500

    added validation.js, factored validation, need to research how to gray out save button and close modal when clicked on the general area of the project

--- README.md
+++ README.md
@@ -7,10 +7,11 @@ Figma
 Images
 Intro
 
+Intro Figma Images Intro
+
 This project is made so all the elements are displayed correctly on popular screen sizes. We recommend investing more time in completing this project, since it's more difficult than previous ones.
 
-Link to the project on Figma
-https://www.figma.com/file/ii4xxsJ0ghevUOcssTlHZv/Sprint-3%3A-Around-the-US?node-id=6432%3A147&t=HIA4dVIlNxmjWc7e-0
+Link to the project on Figma https://www.figma.com/file/ii4xxsJ0ghevUOcssTlHZv/Sprint-3%3A-Around-the-US?node-id=6432%3A147&t=HIA4dVIlNxmjWc7e-0
 
 Images
 
@@ -18,7 +19,9 @@ The way you'll do this at work is by exporting images directly from Figma — we
 
 Good luck and have fun!
 
-My name is Scott Fulmer and am a student at practicum. this project is named Around the U.S. this project entails different media queries on popular screen sizes that is used on our way day-to-day lives. We were tasked on using grids instead of flexboxes and also using percentages and fractions and not fixed pixels like the other 2 projects that weve done.
+My name is Scott Fulmer and this project displays a working website with interactive design.
+you can view the website in any screen size and there wont be any horizontal scrolling and all of its contents will fit according to the screen size.
+you can also Add and remove pictures as you wish, like pictures, and even change the name of the person and the occupation.
 
 You can view my project by following the link below:
 https://scottfulmer.github.io/se_project_aroundtheus/
---
+++ scripts/validation.js
@@ -0,0 +1,78 @@
+// enabling validation by calling enableValidation()
+// pass all the settings on call
+
+function showInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+  const errorMessageEl = formEl.querySelector(`@${inputEl.id}`);
+  inputEl.classList.add(inputErrorClass);
+  errorMessageEl.textContent = inputEl.validationMessage;
+  errorMessageEl.classList.add(errorClass);
+}
+
+function hideInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+  const errorMessageEl = formEl.querySelector(`@${inputEl.id}`);
+  inputEl.classList.remove(inputErrorClass);
+  errorMessageEl.textContent = "";
+  errorMessageEl.classList.remove(errorClass);
+}
+
+function checkInputValidity(formEl, inputEl, options) {
+  if (!inputEl.validity.valid) {
+    return showInputError(formEl, inputEl, options);
+  }
+  hideInputError(formEl, inputEl, options);
+}
+function hasInvalidInput(inputList) {
+  return !inputList.every((inputEl) => inputEl.validity.valid);
+}
+function toggleButtonState(
+  inputElements,
+  submitButton,
+  { inactiveButtonClass }
+) {
+  const foundInvalid = false;
+  inputElements.forEach((inputEl) => {
+    if (!inputEl.validity.valid) {
+      foundInvalid = true;
+    }
+  });
+  if (hasInvalidInput(inputElements)) {
+    submitButton.classList.add(inactiveButtonClass);
+    submitButton.disabled = true;
+    return;
+  }
+  submitButton.classList.remove(inactiveButtonClass);
+  submitButton.disabled = false;
+}
+
+function setEventListeners(formEl, options) {
+  const { inputSelector } = options;
+  const inputElements = [...formEl.querySelectorAll(inputSelector)];
+  const submitButton = formEl.querySelector("modal__button");
+  inputElements.forEach((inputEl) => {
+    inputEl.addEventListener("input", (e) => {
+      checkInputValidity(formEl, inputEl, options);
+      toggleButtonState(inputElements, submitButton, options);
+    });
+  });
+}
+
+function enableValidation(options) {
+  const formElements = [...document.querySelectorAll(options.formSelector)];
+  formElements.forEach((formEl) => {
+    formEl.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+  });
+}
+setEventListeners(formEl, options);
+
+const config = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
+
+enableValidation(config);
